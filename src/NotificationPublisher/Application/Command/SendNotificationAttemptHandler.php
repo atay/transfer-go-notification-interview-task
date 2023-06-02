330b77db1ba1fe1393de57f4d2086d2bcf44df03
Adrian Borkiet <adrian.borkiet@gmail.com>
2023-06-02 16:11:13 +0200

    feat: add logger

--- src/NotificationPublisher/Application/Command/SendNotificationAttemptHandler.php
+++ src/NotificationPublisher/Application/Command/SendNotificationAttemptHandler.php
@@ -5,6 +5,7 @@ namespace App\NotificationPublisher\Application\Command;
 use App\NotificationPublisher\Application\Notification\NotificationAttempt;
 use App\NotificationPublisher\Infrastructure\Notification\TypeProviderFactory;
 use App\NotificationPublisher\Infrastructure\Notification\TypeProviderStrategyBuilder;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -14,6 +15,7 @@ class SendNotificationAttemptHandler implements MessageHandlerInterface
         private MessageBusInterface $bus,
         private TypeProviderFactory $typeProviderFactory,
         private TypeProviderStrategyBuilder $typeProviderStrategyBuilder,
+        private LoggerInterface $logger
     ) {
     }
 
@@ -31,9 +33,11 @@ class SendNotificationAttemptHandler implements MessageHandlerInterface
 
         try {
             $provider->send($notification);
+            $this->logger->info('Notification sent successfully');
         } catch (\Exception $e) {
             $attempt->failAttempt($provider);
             $this->bus->dispatch($attempt);
+            $this->logger->error('Failed to send notification', ['exception' => $e]);
         }
     }
 
--- src/NotificationPublisher/Infrastructure/Notification/Email/Provider/AwsSesEmailProvider.php
+++ src/NotificationPublisher/Infrastructure/Notification/Email/Provider/AwsSesEmailProvider.php
@@ -5,16 +5,25 @@ namespace App\NotificationPublisher\Infrastructure\Notification\Email\Provider;
 use App\NotificationPublisher\Domain\Notification\Notification;
 use App\NotificationPublisher\Infrastructure\Notification\Email\ProviderEnum;
 use App\NotificationPublisher\Infrastructure\Notification\Email\ProviderInterface;
+use App\NotificationPublisher\Infrastructure\Notification\FailedToSendMessage;
+use Psr\Log\LoggerInterface;
 
- class AwsSesEmailProvider implements ProviderInterface 
- {
-    public static function getName(): ProviderEnum {
+class AwsSesEmailProvider implements ProviderInterface
+{
+    public function __construct(
+        private LoggerInterface $logger,
+    ) {
+    }
+    public static function getName(): ProviderEnum
+    {
         return ProviderEnum::AWS_SES;
     }
-    
-    public function send(Notification $notification) {
-        echo "Trying to send email using AWS SES provider...";
-        throw new \Exception();
-        
+
+    public function send(Notification $notification)
+    {
+        $this->logger->info('Trying to send email using AWS SES API');
+        // 2DO implement
+        throw new FailedToSendMessage();
+
     }
 }
